3dd21df5c6dd4b46cb1c88095326b6b4f4f9f1cd
lneuendorf <lneuendorf@wisc.edu>
2025-01-30 18:07:25 -0600

    adding files to host on github pages

--- components/callbacks.py
+++ components/callbacks.py
@@ -1,13 +1,16 @@
-import dash
 from dash import Input, Output, State
 
 def register_callbacks(app):
-    @app.callback(
+    app.clientside_callback(
+        """
+        function(n_clicks, is_open) {
+            if (n_clicks) {
+                return !is_open;
+            }
+            return is_open;
+        }
+        """,
         Output("navbar-collapse", "is_open"),
         [Input("navbar-toggler", "n_clicks")],
         [State("navbar-collapse", "is_open")]
     )
-    def toggle_navbar(n_clicks, is_open):
-        if n_clicks:
-            return not is_open
-        return is_open
