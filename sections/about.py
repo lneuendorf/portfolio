b2be3e8b870c2a3686ac3d900865609b3fa6ff22
lneuendorf <lneuendorf@wisc.edu>
2025-01-31 23:01:46 -0600

    adding disclaimer of work in progress

--- sections/about.py
+++ sections/about.py
@@ -43,6 +43,10 @@ about = (
                     "related side projects, but you can also find me playing disc golf, ultimate "
                     "frisbee, or watching the Packers."
                 ], style={"text-align": "justify"}),
+                # NOTE: this site is a work in progress
+                html.P([
+                    "NOTE: This site is a work in progress. Check back soon for updates!"
+                ], style={"text-align": "justify", "font-style": "italic", "margin-top": "20px"}),
             ], className="text-left")
         ], sm=12, md=7, className="d-flex align-items-center justify-content-center"),
     ],
